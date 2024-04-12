8142ae4c4f7fd600aa483b43b57d34156735bb8e
Ameliaa9 <161509242+Ameliaa9@users.noreply.github.com>
2024-04-12 15:30:20 -0400

    Score
    Now the score increases when player fails to pass the ball.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
     public static GameManager instance;
-    
-    public Rigidbody2D ball; 
+
+    public Rigidbody2D ball;
+    public Text scoreLeftText;
+    public Text scoreRightText;
+
+    private int _scoreLeft;
+    private int _scoreRight;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +21,13 @@ public class GameManager : MonoBehaviour
         if (instance == null)
             instance = this;
 
+        _scoreLeft = 0;
+        _scoreRight = 0;
+
         InitializeBall();
     }
-    public void InitializeBall() 
-    { 
+    public void InitializeBall()
+    {
         float angle = Random.Range(0f, 30f);
         float r = Random.Range(0f, 1f);
         if (r < 0.25f)
@@ -33,7 +42,25 @@ public class GameManager : MonoBehaviour
         ball.velocity = dir.normalized * 10f;
 
         ball.transform.position = Vector3.zero;
-    
+
+    }
+
+    public void IncreaseScore(bool left)
+    {
+        if (left)
+        {
+            _scoreLeft++;
+            scoreLeftText.text = _scoreLeft.ToString();
+
+        }
+        else 
+        {
+            _scoreRight++;
+            scoreRightText.text = _scoreRight.ToString();
+
+        }
+
+
     }
 
 
--- Assets/Scripts/ScoreTrigger.cs
+++ Assets/Scripts/ScoreTrigger.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class ScoreTrigger : MonoBehaviour
 {
+    public bool isLeft;
 
     private void OnTriggerEnter2D(Collider2D collision) 
     {
+        GameManager.instance.IncreaseScore(!isLeft);
         GameManager.instance.InitializeBall();
-    
-    
+
     }
+
 
     
 }
