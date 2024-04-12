47842fe005c889aa9936071cee96ad2cd6b4ea67
Ameliaa9 <161509242+Ameliaa9@users.noreply.github.com>
2024-04-12 14:39:09 -0400

    Making the ball respawn if it hits the triggered area
    if the ball hits the Left or Right walls it will respawn in the middle.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
+
+    public static GameManager instance;
+    
+    public Rigidbody2D ball; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance == null)
+            instance = this;
+
+        InitializeBall();
+    }
+    public void InitializeBall() 
+    { 
+        float angle = Random.Range(0f, 360f) * Mathf.Deg2Rad;
+        Vector2 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        ball.velocity = dir.normalized * 10f;
+
+        ball.transform.position = Vector3.zero;
+    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
