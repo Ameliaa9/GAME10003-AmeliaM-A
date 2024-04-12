1f0ef0fb8fa010713b70a219592162f404cd05fe
Ameliaa9 <161509242+Ameliaa9@users.noreply.github.com>
2024-04-12 14:10:50 -0400

    Paddle movement

--- Assets/Scripts/PaddleManager.cs
+++ Assets/Scripts/PaddleManager.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class PaddleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool isLeft;
+
+    private KeyCode _upKey;
+    private KeyCode _downKey;
+
+    private float _speed = 0.03f;
+
+    //start is called before the first frame update
     void Start()
     {
-        
+        _upKey = isLeft ? KeyCode.W : KeyCode.UpArrow;
+        _downKey = isLeft ? KeyCode.S : KeyCode.DownArrow;
+
     }
 
-    // Update is called once per frame
+    //update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(_upKey)) MoveUp();
+        if (Input.GetKey(_downKey)) MoveDown();
+    }
+
+    void MoveUp()
+    {
+        transform.Translate(Vector2.up * _speed);
+        if (transform.position.y > 4.25f)
+        {
+            Vector3 p = transform.position;
+            p.y = 4.25f;
+            transform.position = p;
+        }
+    }
+
+    void MoveDown()
+    {
+        transform.Translate(-Vector2.up * _speed);
+        if (transform.position.y < -4.25f)
+        {
+            Vector3 p = transform.position;
+            p.y = -4.25f;
+            transform.position = p;
+        }
     }
 }
